20c828f97f9a8763a99aa494496d5c6c88ad7a4e
Oleg Semenov <wemonsh.gg@gmail.com>
2019-04-02 17:27:39 +0300

    contacts

---
+++ _block/pages/contacts/contacts.php
@@ -0,0 +1,86 @@
+<section class="feedback">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h2><span class="line">Как с нами</span>
+                    <span class="line">связаться</span></h2>
+
+                <p class="phone"><strong>Звоните по будням, с 9 до 18:00</strong></br>
+                    <a href="#">7 (495) 782-82-46</a> (Москва)</p>
+
+                <ul class="social-icons">
+                    <li class="icon"><a href="#"><img src="/img/icons/whatsapp.svg" alt=""></a></li>
+                    <li class="icon"><a href="#"><img src="/img/icons/instagram.svg" alt=""></a></li>
+                    <li class="icon"><a href="#"><img src="/img/icons/skype.svg" alt=""></a></li>
+                </ul>
+
+                <p class="address"><strong>Пишите на почту или имейл:</strong></br>
+                    119331, Москва, пр. Вернадского, 29</br>
+                    <a href="#">info@consult-systems.ru</a></p>
+
+                <p><strong>Наши реквизиты:</strong></p>
+                <ul class="requisites">
+                    <li>Лицензия: 123456</li>
+                    <li>ИНН: 7702070139</li>
+                    <li>КПП: 770943002</li>
+                    <li>БИК: 044525411</li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <img src="/img/building.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="passage">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2><span class="line">Как доехать</span></h2>
+                <a class="download" href="#"><img src="/img/icons/download2.png" alt="">Скачать схему проезда</a>
+            </div>
+            <div class="col-12">
+                <div class="map">
+
+                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A750f78651c0fe9e91b03f9d4816d9c10b4472955164559eba048bc05e5cc7216&amp;width=100%25&amp;height=614&amp;lang=ru_RU&amp;scroll=true"></script>
+
+                </div>
+                <h3>Как дойти от станции метро:</h3>
+                <h4><img src="/img/metro.png" alt="">Вернадского</h4>
+                <div class="row path">
+                    <div class="col-6">
+                        <img src="/img/1.png" alt="">
+                        <p>Последний вагон из центра. Выходите из метро, поворачиваете направо и переходите дорогу, далее проехать одну остановку на автобусе №224 до остановки Проспект Вернадского, 33. Прямо напротив остановки находится вход в ТОЦ “Лето” (большие стеклянные двери).</p>
+                    </div>
+                    <div class="col-6">
+                        <img src="/img/2.png" alt="">
+                        <p>Последний вагон из центра. Выходите из метро, поворачиваете направо и переходите дорогу, далее проехать одну остановку на автобусе №224 до остановки Проспект Вернадского, 33. Прямо напротив остановки находится вход в ТОЦ “Лето” (большие стеклянные двери).</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="contacts">
+    <img class="logo" src="img/icons/logo-footer.png" alt="">
+    <div class="social-icons">
+        <ul>
+            <li><a href="#"><img src="img/icons/whatsapp.svg" alt=""></a></li>
+            <li><a href="#"><img src="img/icons/skype.svg" alt=""></a></li>
+            <li><a href="#"><img src="img/icons/youtube.svg" alt=""></a></li>
+            <li><a href="#"><img src="img/icons/instagram.svg" alt=""></a></li>
+        </ul>
+    </div>
+    <span class="headline">Связаться с нами</span>
+    <p class="description">Опытный юрист перезвонит в 11:25 и ответит на вопросы</p>
+    <form action="#">
+        <input type="tel" placeholder="Ваш телефон">
+        <button>Перезвоните мне</button>
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="customControlInline" checked>
+            <label class="custom-control-label" for="customControlInline">Нажимая на кнопку «Отправить заявку», вы соглашаетесь с <a href="#">политикой конфиденциальности</a></label>
+        </div>
+    </form>
+</section>
